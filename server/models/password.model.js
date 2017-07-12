3524657ab0af1ccbd0e755c080f66199afc38ef4
Cyprien Rose <cyprien@MacBook-Pro-de-Cyprien.local>
2017-07-12 14:25:33 +0200

    :leaves: :pencil: Modification of password schema
    Adding userId in password schema

--- server/models/password.model.js
+++ server/models/password.model.js
@@ -9,7 +9,8 @@ import APIError from '../helpers/APIError';
 const PasswordSchema = new mongoose.Schema({
     login: { type: String, required: [true, "can't be blank"] },
     password: { type: String, required: [true, "can't be blank"] },
-    url: { type: String, required: [true, "can't be blank"] }
+    url: { type: String, required: [true, "can't be blank"] },
+    userId: {type: mongoose.Schema.Types.ObjectId, required: true}
 });
 
 
